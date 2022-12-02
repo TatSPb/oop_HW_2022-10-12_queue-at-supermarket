c80b648b8b74d762151ec2046499a252acf39dcf
TatSPb <t-sverchkova@yandex.ru>
2022-12-03 00:09:24 +0300

    Task completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+
 public class Main {
+    private static final List<String> NAMES = List.of(
+            "Maria",
+            "Alexandr",
+            "Petr",
+            "Maxim",
+            "Stas",
+            "Olga",
+            "Anna",
+            "Egor",
+            "Gleb",
+            "Ivan",
+            "Muhozhuk",
+            "Vasia",
+            "Katya",
+            "Alya",
+            "Pavel"
+    );
+
+    private static final Random RANDOM = new Random();
+    public static final int MAX_SIZE = 5;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Queue<String> queue1 = new ArrayDeque<>();
+        Queue<String> queue2 = new ArrayDeque<>();
+        randomFilling(queue1);
+        randomFilling(queue2);
+        System.out.println("First queue: " + queue1);
+        System.out.println("Second queue: " + queue2);
+        System.out.println();
+
+        add ("Polina", queue1, queue2);
+        System.out.println("First queue: " + queue1);
+        System.out.println("Second queue: " + queue2);
+        System.out.println();
+
+        remove (queue1,queue2);
+        System.out.println("First queue: " + queue1);
+        System.out.println("Second queue: " + queue2);
+        System.out.println();
+    }
+
+    private static void add (String name,
+                             Queue<String> queue1,
+                             Queue<String> queue2){
+        if (queue1.size() == MAX_SIZE && queue2.size() == MAX_SIZE){
+            System.out.println("CALLING GALYA!");
+            return;
+        }
+
+        if (queue1.size() < queue2.size()) {
+            queue1.offer(name);
+        } else if (queue2.size() < queue1.size()) {
+            queue2.offer(name);
+        }
+    }
+
+    private static void remove (Queue<String> queue1,
+                                Queue <String> queue2){
+        if (RANDOM.nextBoolean()){
+            queue1.poll();
+        } else {
+            queue2.poll();
+        }
+    }
+    private static void randomFilling (Queue<String> queue) {
+        int size = RANDOM.nextInt (MAX_SIZE + 1);
+        for (int i = 0; i < size; i++) {
+            queue.offer(NAMES.get(RANDOM.nextInt(NAMES.size())));
+        }
     }
 }
